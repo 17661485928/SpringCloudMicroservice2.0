91663704556e4cde1a031b9858f8c96de9bf121f
17661485928 <17661485928@163.com>
2019-04-25 17:48:31 +0800

    提交Spring Boot-Consumer消费者服务应用修该记录

--- springboot-consumer/src/main/java/com/qht/springbootconsumer/SpringbootConsumerApplication.java
+++ springboot-consumer/src/main/java/com/qht/springbootconsumer/SpringbootConsumerApplication.java
@@ -1,6 +1,5 @@
 package com.qht.springbootconsumer;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
