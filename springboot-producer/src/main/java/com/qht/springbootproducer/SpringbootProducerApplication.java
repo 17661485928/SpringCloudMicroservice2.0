ccd6d37dda87b0f3b838c2b0fa3abc05dca453a0
17661485928 <17661485928@163.com>
2019-04-25 15:59:35 +0800

    SpringBoot2.0-Producer生产者服务应用

---
+++ springboot-producer/src/main/java/com/qht/springbootproducer/SpringbootProducerApplication.java
@@ -0,0 +1,18 @@
+package com.qht.springbootproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * SpringBoot-Producer服务
+ * @author qht
+ * @date 2019/4/25
+ * @return
+ */
+@SpringBootApplication
+public class SpringbootProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootProducerApplication.class, args);
+    }
+
+}
