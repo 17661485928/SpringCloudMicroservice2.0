d312b64bd249ac45d856e00666e8e41f6b28bfa3
17661485928 <17661485928@163.com>
2019-04-25 16:58:10 +0800

    提交Spring Boot-Consumer消费者服务应用

---
+++ springboot-consumer/src/main/java/com/qht/springbootconsumer/SpringbootConsumerApplication.java
@@ -0,0 +1,18 @@
+package com.qht.springbootconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * SpringBoot-Consumer消费者服务
+ * @author qht
+ * @date 2019/4/25
+ * @return
+ */
+@SpringBootApplication
+public class SpringbootConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootConsumerApplication.class, args);
+    }
+
+}
